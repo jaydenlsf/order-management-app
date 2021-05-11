21e0316b88e11e22a098f95b71e60decb47e5433
jayden-lee <jaydenlsf47@gmail.com>
2021-05-11 17:05:07 +0100

    Added route for adding order

--- application/models.py
+++ application/models.py
@@ -1,4 +1,6 @@
 from application import db
+from datetime import datetime
+import pytz
 
 
 class User(db.Model):
@@ -8,6 +10,7 @@ class User(db.Model):
     house_number = db.Column(db.String(12), nullable=False)
     postcode = db.Column(db.String(10), nullable=False)
     phone = db.Column(db.String(11), nullable=False)
+    orders = db.Column(db.String(30))
 
 
 class Order(db.Model):
@@ -15,5 +18,9 @@ class Order(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     order_status = db.Column(db.String(30), default="Order placed")
     tracking_num = db.Column(db.String(12))
-    date = db.Column(db.Date)
-    time = db.Column(db.Time)
+    date = db.Column(
+        db.Date, default=datetime.now(pytz.timezone("Europe/London")).date()
+    )
+    time = db.Column(
+        db.Time, default=datetime.now(pytz.timezone("Europe/London")).time()
+    )
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,42 @@
 from application import db, app
+from flask import render_template, redirect, request, url_for, flash
+from application.models import User, Order
+from application.forms import UserForm
+from sqlalchemy.exc import IntegrityError
 
 
 @app.route("/")
 def home():
-    return "Welcome to the homepage."
+    users = User.query.all()
+    return render_template("index.html", users=users)
+
+
+@app.route("/add-order", methods=["GET", "POST"])
+def add_order():
+    form = UserForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            try:
+                user = User(
+                    email=form.email.data,
+                    name=form.name.data,
+                    house_number=form.house_number.data,
+                    postcode=form.postcode.data,
+                    phone=form.phone.data,
+                )
+                db.session.add(user)
+                order = Order(
+                    customer_id=User.query.filter_by(email=form.email.data).first().id
+                )
+                db.session.add(order)
+                db.session.commit()
+                return redirect(url_for("home"))
+            except IntegrityError:
+                flash("Please enter a different email address.")
+
+    return render_template("add-order.html", title="Add Order", form=form)
+
+
+@app.route("/update-order", methods=["GET", "POST"])
+def update_order():
+    return render_template("update-order.html", title="Update Order")
