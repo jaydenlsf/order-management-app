373c74f61a882976836a3d33e92e76816041c24d
jayden-lee <jaydenlsf47@gmail.com>
2021-05-11 15:57:04 +0100

    Initialised app

---
+++ application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+
+db = SQLAlchemy(app)
+
+from application import routes
