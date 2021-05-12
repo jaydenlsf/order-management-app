b06789d6f299ce10757fb7c6443f98115197d8a8
jayden-lee <jaydenlsf47@gmail.com>
2021-05-12 16:56:18 +0100

    Minor changes

--- application/models.py
+++ application/models.py
@@ -1,20 +1,21 @@
 from application import db
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(320), nullable=False)
     name = db.Column(db.String(30), nullable=False)
     house_number = db.Column(db.String(12), nullable=False)
     postcode = db.Column(db.String(10), nullable=False)
     phone = db.Column(db.String(11), nullable=False)
-    order_numbers = db.Column(db.String(30), default="No order")
+    order_numbers = db.Column(db.String(30), default="no order")
+    orders = db.relationship("Orders", backref="users")
 
 
-class Order(db.Model):
+class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    order_status = db.Column(db.String(30), default="Order placed")
+    customer_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    order_status = db.Column(db.String(30), default="order placed")
     tracking_num = db.Column(db.String(12))
     date = db.Column(db.Date)
     time = db.Column(db.Time)
